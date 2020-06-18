684ae04447d6beb73a9de7ec8c61cbcf87a36fe3
kaustubhr <kaustubh.r.deshpande@gmail.com>
2020-06-18 00:00:53 -0500

    EventEmitter (on is like listener and emit will trigger the event listener)

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var stuff = require('./stuff');
-
+var event = require('events');
+var util = require('util');
 console.log(__dirname);
 console.log(__filename);
 var t=0;
@@ -33,3 +34,42 @@ function callFunction(fun)
 {
     fun();
 }
+
+//event emitter example
+var myEmitter = new event.EventEmitter();
+//below is the listener
+myEmitter.on('someEvent',function(msg){
+    console.log(msg);
+});
+
+//now we can actually emit the event
+myEmitter.emit('someEvent','we are emitting through event-emitter'); 
+
+//creates class that inherits events.EventsEmitter
+class Person extends event.EventEmitter{
+    constructor(name){
+        super();
+        
+    this.name = name;
+    }
+}
+
+//EventEmitter is inherited into Person object now
+//util.inherits(Person,event.EventEmitter);
+
+var person1 = new Person('kaustubh');
+var person2 = new Person('rahul');
+var person3 = new Person('desai');
+
+var peopleArray = [person1,person2,person3];
+//for each person in peopleArray, attach an event listener (emitter)
+peopleArray.forEach(function(person){
+    person.on('speak',function(msg){
+        console.log(`${person.name} said : ${msg}` );
+    });
+});
+
+//calling emit on each Person object. Person has util.inherited EventEmitter functionality  
+peopleArray.forEach(function(person){
+    person.emit('speak'," hello ");
+});
