788f7acfc2e9484c8417cf78cd363af688b25643
kaustubhr <kaustubh.r.deshpande@gmail.com>
2020-06-18 22:34:55 -0500

    basic routing (Added error page and api page)

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(function(req,res){
     console.log('request was made : '+ req.url);
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hey people');
+    if(req.url === '/head' || req.url === '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);    
+    }
+    else if(req.url == '/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res); 
+    }
+    else if(req.url==='/api'){
+        res.writeHead(200,{'Content-Type':'application/JSON'});
+        var myObj = {
+            name: 'Kaustubh',
+            job: 'Software Developer',
+            age:'24'
+        };
+        //expects buffer or string
+        res.end(JSON.stringify(myObj));
+    }   
+    //res.writeHead(200,{'Content-Type': 'application/JSON'});
+    //res.end expects string of buffer
+    //res.end(JSON.stringify(myObj));
+
+    else{
+        res.writeHead(404,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname + '/error.html').pipe(res); 
+    }
 
 });
 
